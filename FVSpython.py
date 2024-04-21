4cd9c0a033afbd9a1d9b4a7b82459b81463564a1
NOAM_DIAMANT <noamd1234@gmail.com>
2024-04-21 14:43:42 +0300

    finished inputfile func

--- FVSpython.py
+++ FVSpython.py
@@ -1,20 +1,30 @@
-import os
-import numpy as np
+import os  # Import the os module for file operations
+import numpy as np  # Import numpy module
 
+# Function to input the file name and validate it
 def inputFileName():
     while (True):
+        # Prompt the user to input the path to the input file
         print("Please input the path to the input file, ends with extension \".xsb\": ")
+        
+        # Read the input from the user
         FileName = str(input())
+        
+        # Check if the file name ends with ".xsb"
         if not (FileName.endswith(".xsb")):
-            print("Invalid input, try again")
+            print("Invalid input, try again!")  # Print error message for invalid input
         else:
-            break
-    FileNameForRead = ""
-    for char in FileName:
-        if char == "\\":
-            FileNameForRead += "\\"
-        FileNameForRead += char
-    return FileNameForRead
+            FileNameForRead = ""
+            # Process the file name to handle escape characters
+            for char in FileName:
+                if char == "\\":
+                    FileNameForRead += "\\"  # Escape the backslash character
+                FileNameForRead += char
+            # Check if the file exists
+            if (os.path.exists(FileNameForRead)):
+                return FileNameForRead  # Return the validated file name
+            else:
+                print("File does not exist, try again!")  # Print error message for non-existent file
 
 def readFile(FileName):
         with open(FileName, 'r') as f:
@@ -39,8 +49,8 @@ class SokobanBoard:
 
 if __name__ == '__main__':
     print("Hello, and welcome to the Sokoban folder!")
-    #FileNameForRead = inputFileName()
+    FileNameForRead = inputFileName()
     b = SokobanBoard(3,5)
     b.printBoard()
-    #readFile(FileNameForRead)
+    readFile(FileNameForRead)
 
